71d6871e6737112fb128086f7fce8a1c0c2ad4b0
allurisriakhil <allurisriakhil@gmail.com>
2018-11-09 14:10:44 -0500

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,19 @@
+var Book = require('./Book');
+var Magazine = require('./Magazine');
+var Music = require('./Music');
+var Movie = require('./Movie');
+var Admin = require('./Admin');
+var Student = require('./Student');
+
+
+
+module.exports = {
+    Book:Book,
+    Magazine:Magazine,
+    Music:Music,
+    Movie:Movie,
+    Admin:Admin,
+    Student:Student,
+};
+
+
