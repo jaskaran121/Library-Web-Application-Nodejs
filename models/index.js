51e5cfc65a2b3e49f5fcd57edc3695cd572999ad
Jaskaran Kukreja <35613378+Jaskaran121@users.noreply.github.com>
2018-11-23 18:58:50 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -4,16 +4,15 @@ var Music = require('./Music');
 var Movie = require('./Movie');
 var Admin = require('./Admin');
 var Student = require('./Student');
-
+var Person = require('./Person');
 
 
 module.exports = {
-    Book:Book,
-    Magazine:Magazine,
-    Music:Music,
-    Movie:Movie,
-    Admin:Admin,
-    Student:Student,
+    Book: Book,
+    Magazine: Magazine,
+    Music: Music,
+    Movie: Movie,
+    Admin: Admin,
+    Student: Student,
+    Person: Person
 };
-
-
