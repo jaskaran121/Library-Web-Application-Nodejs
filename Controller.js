c9a4fc6e097d17c96a96df65ad0db8be3bfe083d
claudiiag <claudiiaguerreiro@hotmail.com>
2018-11-23 03:36:19 -0500

    Update Controller.js

--- Controller.js
+++ Controller.js
@@ -219,6 +219,16 @@ app.post('/api/update/:entry/:id', (req, res) => {
             })
 
     }
+	if (req.params.entry === 'Movie') {
+        mapper.update_Movie(req.body.Title, req.body.Director, req.body.Producers, req.body.Actors, req.body.Language,
+            req.body.Subtitles, req.body.Dubbed, req.body.ReleaseDate, req.body.RunTime, req.params.id, function (type) {
+                if (type === 'success')
+                    res.status(200).json({ "success": 'SOEN 341' });
+                else
+                    res.status(500).json({ "error": 'Not able to update values' });
+            });
+
+    }
 	
 	if (req.params.entry === 'Music') {
         mapper.update_Music(req.body.Type, req.body.Title, req.body.Artist, req.body.Label,
