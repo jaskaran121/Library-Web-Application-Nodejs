5590a11f212263a8db565cdc16c2b661e528e1e7
Himalaya Yadav Reddiboina <himalayayadav.reddiboina@gmail.com>
2018-11-23 12:13:41 -0500

    updated magazine.js

--- models/Magazine.js
+++ models/Magazine.js
@@ -1,7 +1,7 @@
 const PrintedItem = require('./PrintedItem');
 class Magazine extends PrintedItem{
     constructor(Title, Language, Publisher, ISBN10, ISBN13,id) {
-        super(Title, Language, Publisher, ISBN10, ISBN13,null);
+        super(Title, Language, Publisher, ISBN10, ISBN13,id);
     }
     
 }
