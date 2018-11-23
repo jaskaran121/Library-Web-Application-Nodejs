02e54c8ae3bdf70d81a03541a6845f1b0088e882
Elderbi <seif.derby@yahoo.com>
2018-11-23 16:48:22 -0500

    admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,65 +1,11 @@
-const db = require('../db-connector');
+const Person = require('./Person');
 
-class Admin{
-constructor(FirstName,LastName,user,password,Email,PhoneNumber){
-    this.FirstName = FirstName;
-    this.LastName = LastName;
-    this.user = user;
-    this.password = password;
-    this.Email = Email;
-    this.PhoneNumber = PhoneNumber
-}
-
-admin_Info()
-{
-    return{
-        FirstName:this.FirstName,
-        LastName:this.LastName,
-        user:this.user,
-        password:this.password,
-        Email:this.Email,
-        PhoneNumber:this.PhoneNumber
+class Admin extends Person{
+    
+    constructor(FirstName, LastName, user, password, Email, PhoneNumber) {
+        super(FirstName,LastName,user,password,Email,PhoneNumber);
     }
 }
-
-insert(callback){
-    db.getInstance().query(`INSERT INTO users(FirstName,LastName,user,password,Email,PhoneNumber)
-    VALUES('${this.FirstName}','${this.LastName}','${this.user}',
-    '${this.password}','${this.Email}','${this.PhoneNumber}')`,function(err,result){
-        if(result.affectedRows){
-            callback('success')
-        }
-        else
-        callback('error');
-    });
-}
-
-static login(user,password,callback)
-{
-db.getInstance().query(`SELECT * FROM users WHERE user = '${user}'`,function(err,result){
-    if(result.length>0){
-        if(result[0].password===password)
-        callback('success');
-        else
-        callback('error')
-    }
-    else
-    callback('error1');
-});
-}
-
-static viewUsers(callback)
-{
-db.getInstance().query(`SELECT * FROM students`,function(err,result){
-    if(result.length>0)
-    {
-        callback('success',result);
-    }
-    else{
-        callback('error',null);
-    }
-})
-}
-}
+    
 
 module.exports = Admin;
