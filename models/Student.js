de39dff5f6585cbadd4b08a03adc03d808d742ff
nayanarajc <nayanaraj.237@gmail.com>
2018-11-22 22:23:16 -0500

    nayana update

--- models/Student.js
+++ models/Student.js
@@ -1,57 +1,10 @@
-const db = require('../db-connector');
+const Person = require('./Person');
 
-class Student{
-   constructor(FirstName,LastName,UserName,Password,Email,PhoneNumber){
-      this.FirstName = FirstName;
-      this.LastName = LastName;
-      this.UserName = UserName;
-      this.Password = Password;
-      this.Email = Email;
-      this.PhoneNumber = PhoneNumber
-    }
-
-    student_Info()
-    {
-        return{
-            FirstName:this.FirstName,
-            LastName:this.LastName,
-            UserName:this.UserName,
-            Password:this.LastName,
-            Email:this.Email,
-            PhoneNumber:this.PhoneNumber
-        }
-    }
-
-    insert(callback)
-    {
-           db.getInstance().query(`INSERT INTO students(FirstName,LastName,UserName,Password,Email,PhoneNumber)
-           VALUES('${this.FirstName}','${this.LastName}','${this.UserName}',
-           '${this.Password}','${this.Email}','${this.PhoneNumber}')`,function(err,result){
-               if(result.affectedRows)
-               {
-                   callback('success');
-               }
-               else{
-                   callback('error');
-               }
-           });
-    }
-
-    static login(user,password,callback)
-{
-db.getInstance().query(`SELECT * FROM students where UserName='${user}'`,function(err,res){
-    if(res.length>0){
-        if(res[0].Password===password)
-        callback('success');
-        else
-        callback('error');
-    }
-    else
-    callback('error1');
-});
-}  
+class Student extends Person{
+    
+    constructor(FirstName, LastName, UserName, Password, Email, PhoneNumber) {
+        super(FirstName,LastName,UserName,Password,Email,PhoneNumber);
+    } 
 }
 
 module.exports = Student;
-
-    
